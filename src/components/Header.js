589fc566da57a77eab9104de98ba884e7c86942c
bjabot12 <35260055+bjabot12@users.noreply.github.com>
2019-11-04 07:44:12 +0100

    finished loading projects from projects.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from "react"
+import React, { Component } from "react"
 import HeaderItem from "./HeaderItem"
 import styled from "styled-components"
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import myProjects from './../projects.json';
+
  
 const Container = styled.div`
 height: 50em;
@@ -10,17 +11,29 @@ text-align: center;
 
 const Projects = () => {
 
-  console.log(myProjects)
+  const proj = useState(myProjects)
+  const p = proj[0]
+  console.log()
   return (
     <Container>
-      <h2>Prosjekter</h2>
+      {p ? (
+        <React.Fragment>
+          {p.projects.map(proj => <div key={proj.id}>
+            {console.log(proj)}
+            <h2>{proj.title}</h2>
+            <p>{proj.descr}</p>
+          </div>)}
+      </React.Fragment>)
+    :
+    <h1>nothing</h1>}
+      {/* <h2>Prosjekter</h2>
       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-        culpa qui officia deserunt mollit anim id est laborum."</p>
+        culpa qui officia deserunt mollit anim id est laborum."</p> */}
     </Container>
   )
 
